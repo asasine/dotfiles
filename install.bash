d831e4561c4e5d317369189310ef266cb320bce6
Adam Sasine <adsasine@microsoft.com>
2021-04-08 11:51:59 -0400

    Added setup scripts to install.bash

--- install.bash
+++ install.bash
@@ -1,26 +1,46 @@
 #!/bin/bash
 
 # link install files to ~
-for file in $(ls -A install); do
+base_dir="install"
+echo "Linking dotfiles from $base_dir to ~"
+for file in $(ls -A $base_dir); do
     # check if file already exists and its not a symlink
     if [ -f ~/$file -a ! -h ~/$file ]; then
         echo "Moving ~/$file to ~/$file.bak"
         mv -v ~/$file{,.bak}
     fi
 
-    echo "Creating symlink to install/$file in home directory."
-    ln -svf ~/dotfiles/install/$file ~/$file
+    echo "Creating symlink to $base_dir/$file in home directory."
+    ln -svf ~/dotfiles/$base_dir/$file ~/$file
 done
 
 # link config directories to ~/.config
-for file in $(ls -A config); do
-    if [ -d ~/.config/$file -a ! -h ~/.config/$file ]; then
-        echo "Moving ~/.config/$file to ~/.config/$file.bak"
-        mv -v ~/.config/$file{,.bak}
+base_dir="config"
+echo
+echo "Linking config files from $base_dir to ~/.$base_dir"
+for file in $(ls -A $base_dir); do
+    if [ -d ~/.$base_dir/$file -a ! -h ~/.$base_dir/$file ]; then
+        echo "Moving ~/.$base_dir/$file to ~/.$base_dir/$file.bak"
+        mv -v ~/.$base_dir/$file{,.bak}
     fi
 
-    echo "Creating symlink to config/$file/ in ~/.config"
-    ln -svf ~/dotfiles/config/$file ~/.config/$file
+    echo "Creating symlink to $base_dir/$file/ in ~/.$base_dir"
+    ln -svf ~/dotfiles/$base_dir/$file ~/.$base_dir/$file
 done
 
-unset $file
+# run scripts in setup
+base_dir="setup"
+echo
+echo "Running all executable scripts in $base_dir"
+for file in $(ls -A $base_dir); do
+    if [ -x $base_dir/$file ]
+    then
+        echo "Running ./$base_dir/$file"
+        ./$base_dir/$file
+    else
+        echo "$base_dir/$file is not executable"
+    fi
+done
+
+unset base_dir
+unset file
---
+++ setup/install_pip.bash
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo "Installing Python3 packages"
+pip3 install -U quantiphy
