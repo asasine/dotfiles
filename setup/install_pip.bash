bef114b88e71454cf634b25f1c103a5efb9ab506
Adam Sasine <adsasine@microsoft.com>
2021-08-11 09:05:03 -0400

    Added git installation

--- setup/install_pip.bash
+++ setup/install_pip.bash
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+echo "Installing latest git from git's PPA"
+sudo add-apt-repository -y --no-update ppa:git-core/ppa
+sudo apt-get update
+sudo apt-get install -y git
+
 echo "Installing Python3 packages"
 pip3 install -U quantiphy pyperclip
