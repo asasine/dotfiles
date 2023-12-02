6ec0766c33def9b734ca0fae1c62486fb174a1d8
Adam Sasine <adam.sasine@gmail.com>
2023-12-02 05:58:57 -0500

    Added bisect command

--- setup/11_install_pip_packages.bash
+++ setup/11_install_pip_packages.bash
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 echo "Installing Python3 packages"
-python3 -m pip install -U more_itertools pyperclip python-dateutil quantiphy rich
+python3 -m pip install -U \
+    click \
+    more_itertools \
+    pyperclip \
+    python-dateutil \
+    quantiphy \
+    rich
