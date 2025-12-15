5d3196b21f024360b8a1c29b9723693069294189
Adam Sasine <adam.sasine@gmail.com>
2025-12-15 14:06:59 -0800

    Add git config installation

--- install.ps1
+++ install.ps1
@@ -1,3 +1,12 @@
 $destination = Split-Path -Parent $PROFILE.CurrentUserAllHosts
 New-Item -Path $destination\Profile -ItemType SymbolicLink -Value $PSScriptRoot\pwsh\Profile -Force
 New-Item -Path $PROFILE.CurrentUserAllHosts -ItemType SymbolicLink -Value $PSScriptRoot\pwsh\profile.ps1 -Force
+
+# make .config dir and symlink config directories
+New-Item -Path $HOME\.config -ItemType Directory -Force
+$config_dirs = Get-ChildItem -Path $PSScriptRoot\config -Directory
+foreach ($dir in $config_dirs) {
+    $source = Join-Path -Path $PSScriptRoot\config -ChildPath $dir.Name
+    $target = Join-Path -Path $HOME\.config -ChildPath $dir.Name
+    New-Item -Path $target -ItemType SymbolicLink -Value $source -Force
+}
